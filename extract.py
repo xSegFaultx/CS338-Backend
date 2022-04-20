3743ed012e8c48b3236b7c75b58c79a0ef94e8e4
xSegFaultx <leodre1022@hotmail.com>
2022-04-19 21:14:10 -0500

    Image search V1.0

--- extract.py
+++ extract.py
@@ -1,4 +1,6 @@
 from util import *
+import requests
+import os
 
 num2themes = ['music', 'movies', 'sports', 'fashion', 'food', 'technology']
 themes2num = {}
@@ -16,7 +18,17 @@ theme_query = [r'most popular music genre in {}s',
 def get_theme_image(theme, birth_year):
     decade = get_decade(int(birth_year)) + 20
     theme_num = themes2num[theme]
-    query = theme_query[theme_num].format(decade)
-    return query2image(query)
+    query = "_".join(theme_query[theme_num].format(decade).split())
+    image_link = query2image(query)
+    response = requests.get(image_link)
+    if response.status_code != 200:
+        return -1
+    if not os.path.exists('./images'):
+        os.makedirs('./images')
+    with open("./images/theme.png", 'wb') as image_file:
+        image_file.write(response.content)
+    return 0
 
 
+# testing
+get_theme_image('music', '1995')
--- util.py
+++ util.py
@@ -1,6 +1,28 @@
+import os
+from dotenv import load_dotenv
+import requests
+import numpy as np
+
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
+ENGINE_ID = os.getenv("ENGINE_ID")
+SEARCH_TYPE = "image"
+NUMBER = 10
+FILE_TYPE = 'png'
+
+
 def get_decade(year):
     return int(year / 10) * 10
 
 
 def query2image(query):
-    return query
+    google_image_api = r"https://www.googleapis.com/customsearch/v1?key={}&cx={}&searchType={}&fileType={}&num={}&q={}" \
+        .format(API_KEY, ENGINE_ID, SEARCH_TYPE, FILE_TYPE,NUMBER, query)
+    response = requests.get(google_image_api)
+    if "items" in response.json():
+        result = response.json()["items"]
+        choice = np.random.choice(len(result))
+        image_link = result[choice]['link']
+        return image_link
+    else:
+        print(response.json())
